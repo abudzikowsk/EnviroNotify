b650b918f2e057119cc6d4a2e18bb96d1f3702e0
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2023-12-26 22:25:43 +0100

    add initial raspberrypi program

---
+++ EnviroNotify.RaspberryPi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Device.I2c;
+using Iot.Device.Shtc3;
+
+var settings = new I2cConnectionSettings(1, Shtc3.DefaultI2cAddress);
+var device = I2cDevice.Create(settings);
+
+using var sensor = new Shtc3(device);
+Console.WriteLine($"SensorId {sensor.Id}");
+
+while (true)
+{
+    if (sensor.TryGetTemperatureAndHumidity(out var temperature, out var humidity))
+    {
+        Console.WriteLine($"Temperature {temperature.DegreesCelsius}");
+        Console.WriteLine($"Humidity {humidity.Percent}");
+    }
+    
+    sensor.Sleep();
+    Thread.Sleep(1000);
+}
